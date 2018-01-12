b9d5b59dcec528db7c74e77241e486017142eeeb
Minjae Son <bungabear6422@gmail.com>
2018-01-12 11:33:27 +0900

    RTCDataChannel이 아닌 ws를 이용한 채팅 기능 실습.

---
+++ src/chat/moWebsocket.js
@@ -0,0 +1,39 @@
+var g_mc_ws_component = {
+    ws: null,
+    //url: 'ws://127.0.0.1:3000/signal",
+    url: 'wss://minjae0webrtc.herokuapp.com/signal',
+    init: function () {
+
+    },
+    connect: function (url, onExternalMessage) {
+        this.url = url || this.url;
+        
+        this.ws = new WebSocket(this.url);
+        this.ws.onopen = this.onConnected;
+        this.ws.onmessage = onExternalMessage || this.onMessage;
+        this.ws.onclose = this.onClosed;
+    },
+    sendMessage: function (txt) {
+        if (this.ws != null) {
+            this.ws.send(txt);
+        } else {
+            alert('connection not established, please connect.');
+        }
+    },
+    disconnect: function () {
+        if (this.ws != null) {
+            this.ws.close();
+            this.ws = null;
+        }
+    },
+    onConnected: function () {
+        console.info('Info: connection opened.');
+    },
+    onMessage: function (event) {
+        console.info('Received: ' + event.data);
+    },
+    onClosed: function (event) {
+        console.info('Info: connection closed.');
+        console.info(event);
+    }
+};
